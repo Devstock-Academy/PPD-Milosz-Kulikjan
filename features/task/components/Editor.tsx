28522c6b77bb818a03be57b2cafde127fef7e833
KulMilosz <kulikjan.milosz@gmail.com>
2026-01-03 00:12:22 +0100

    ready to check

--- features/task/components/Editor.tsx
+++ features/task/components/Editor.tsx
@@ -2,12 +2,66 @@
 
 import React from 'react'
 import { useTranslations } from 'next-intl'
+import MonacoEditor, { BeforeMount } from '@monaco-editor/react'
 
 import TabSkeleton from './TabSkeleton'
+import EditorActions from './EditorActions'
 
 const Editor = () => {
+  const [hasErrors, setHasErrors] = React.useState(false)
+
   const t = useTranslations('Task')
-  return <TabSkeleton tabs={[{ label: t('editor') }]}>Edytor kodu</TabSkeleton>
+
+  const handleValidation = (markers: any[]) => {
+    const monacoErrors = markers.length > 0
+    setHasErrors(monacoErrors)
+  }
+
+  const handleBeforeMount: BeforeMount = (monaco) => {
+    monaco.editor.defineTheme('taskTheme', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [],
+      colors: {
+        'editor.background': '#222426',
+        'editorGutter.background': '#333537',
+        'editorLineNumber.foreground': '#BDBDBD',
+        'editorLineNumber.activeForeground': '#FFFFFF',
+        'scrollbarSlider.background': '#ffffff',
+        'scrollbarSlider.hoverBackground': '#f0f0f0',
+        'scrollbarSlider.activeBackground': '#ffffff',
+        'scrollbar.shadow': '#222426',
+      },
+    })
+  }
+
+  return (
+    <TabSkeleton tabs={[{ label: t('editor') }]}>
+      <div className='flex h-full w-full flex-col'>
+        <div className='flex-1 overflow-hidden rounded shadow-tabBarShadow'>
+          <MonacoEditor
+            language='javascript'
+            height='100%'
+            width='100%'
+            theme='taskTheme'
+            beforeMount={handleBeforeMount}
+            onValidate={handleValidation}
+            options={{
+              minimap: { enabled: false },
+              scrollbar: {
+                vertical: 'auto',
+                horizontal: 'auto',
+                verticalScrollbarSize: 6,
+                horizontalScrollbarSize: 6,
+              },
+              lineNumbersMinChars: 3,
+            }}
+          />
+        </div>
+        <EditorActions hasErrors={hasErrors} />
+      </div>
+    </TabSkeleton>
+  )
 }
 
 export default Editor
--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,7 @@ module.exports = {
         75: '18.75rem', // 300px
         55: '13.75rem', // 220px
         40: '10rem', // 160px
+        175: '43.75rem', // 700px
         'icon-sm': '0.75rem', // 12px
         'icon-base': '1.25rem', // 20px
         'icon-lg': '1.5rem', // 24px
