7a1a8ae2c34ff30ad6572933ad2ff4461c7002cb
KulMilosz <kulikjan.milosz@gmail.com>
2026-02-04 13:52:20 +0100

    fix grid/slider and css/html errors

--- app/[locale]/(private)/css-task/[id]/page.tsx
+++ app/[locale]/(private)/css-task/[id]/page.tsx
@@ -54,7 +54,12 @@ const CssTaskPage = () => {
                   withoutTab
                   taskType='css'
                 />,
-                <Editor key='editor' withoutTab withoutActions />,
+                <Editor
+                  key='editor'
+                  withoutTab
+                  withoutActions
+                  language='html'
+                />,
               ]}
             </TabSkeleton>
           </div>
